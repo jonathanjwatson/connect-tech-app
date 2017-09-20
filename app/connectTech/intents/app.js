d1360aea4321206da68bffc19c59f0b6d923683e
jwats287 <jwats287@users.noreply.github.com>
2017-09-20 12:39:00 -0400

    Tracks

--- app/connectTech/intents/app.js
+++ app/connectTech/intents/app.js
@@ -52,9 +52,9 @@ let connectTech = function (app) {
          let name = request.slot('NAME');
          return app.audiofiles.getTracklist(name)
          .then( (playlist) => {
-             console.log(playlist)
-             let track = playlist.items.preview_url,
-                 trackName = playlist.items.name,
+             console.log(playlist.tracks.items)
+             let track = playlist.tracks.items.preview_url,
+                 trackName = playlist.tracks.items.name,
                  audioPlayerPayload = {
                     url: track,
                     token: trackName,
